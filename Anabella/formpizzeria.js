2cf6c057092c739021628b7bc97f08b9efde6459
anabella.aceto <anabella.aceto@gmail.com>
2023-10-18 16:01:22 +0200

    formulario de pizzaría en html, algo de función para validar

--- Anabella/formpizzeria.js
+++ Anabella/formpizzeria.js
@@ -1,42 +1,51 @@
-function pedirPizza(){
+
+    function cargarEstilo(){
+    
     //Agrego algo de estilo a la página
-    document.body.style.textAlign="center";
+    document.body.style.textAlign="left";
     document.body.style.width="50%";
     document.body.style.margin="auto";
     document.body.style.backgroundColor="#F6BB69";
+    }
 
-    ////Crear el objeto formulario
-		let formulario=document.createElement("form");
- 
-		function crearContenedor(labelText, inputId, inputName, inputType, inputTitle) {
+    function validarForm() {
+    var nombre = document.getElementById("nombre").value;
+    var apellidos = document.getElementById("apellidos").value;
+    var direccion = document.getElementById("direccion").value;
+    var telefono = document.getElementById("telefono").value;
 
-            let contenedor = document.createElement("div");
-    
-            let label = document.createElement("label");
-            label.setAttribute("for", inputId);
-            label.textContent = labelText;
-    
-            let input = document.createElement("input");
-            input.type = inputType;
-            input.id = inputId;
-            input.name = inputName;
-            input.required = true;
-            input.title = inputTitle;
-    
-            contenedor.appendChild(label);
-            contenedor.appendChild(input);
-            contenedor.style.padding = "5px";
-            contenedor.style.marginBottom="10px";
-    
-            return contenedor;
+    if (nombre.trim() === "" || apellidos.trim() === "" || direccion.trim() === "" || telefono.trim() === "") {
+        alert("[ERROR] Todos los campos de texto deben estar rellenos.");
+        return false;
+    }
+    else
+      return true;
+}
+
+  //Validando qie se haya elegido una opción para el radio  
+  tamanio = document.getElementById("tamanio");
+  var seleccion = false;
+        for(var i=0; i<tamanio.length; i++) {
+            if(tamanio[i].checked) {
+                seleccion = true;
+                break;
+            }
         }
     
-        // Agregar campos de entrada al formulario
-        formulario.appendChild(crearContenedor("Nombre:", "nombre", "nombre", "text", "En este espacio debes escribir tu nombre"));
-        formulario.appendChild(crearContenedor("Apellidos:", "apellidos", "apellidos", "text", "En este espacio debes escribir tu/s apellido/s"));
-        formulario.appendChild(crearContenedor("DNI/NIE:", "dni", "dni", "text", "En este espacio debes escribir tu número de DNI o NIE"));
-        formulario.appendChild(crearContenedor("Teléfono:", "tel", "tel", "text", "En este espacio debes escribir tu número de móvil"));
-        formulario.appendChild(crearContenedor("email:", "email", "email", "text", "En este espacio debes escribir tu dirección de correo electrónico"));
-	}
- 
-    
+       if(!seleccionado) {
+          alert("[ERROR] Debe seleccionar pequeña, mediana o grande)");
+          return false;
+      }
+
+
+window.onload = function () {
+    var formulario = document.getElementById("formulario");
+    formulario.onsubmit = validarForm;
+    cargarEstilo();
+
+    }
+
+    function mostarPrecio(){
+
+      
+    }
