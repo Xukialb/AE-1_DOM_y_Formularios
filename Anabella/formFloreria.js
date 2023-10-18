72fa58062320aa71b3fbfa17fc1e92cdf1e2ea41
anabella.aceto <anabella.aceto@gmail.com>
2023-10-18 18:32:47 +0200

    validación funciona

---
+++ Anabella/formFloreria.js
@@ -0,0 +1,14 @@
+function accederForm() {
+    
+    // Agrego algo de estilo a la página
+    document.body.style.textAlign = "left";
+    document.body.style.width = "30%";
+    document.body.style.margin = "auto";
+    document.body.style.backgroundColor = "#EEE6EE";
+
+    // Creo el formulario
+    var formulario = document.createElement("form");
+    formulario.style.border = "solid pink";
+
+    
+}
--- Anabella/formpizzeria.js
+++ Anabella/formpizzeria.js
@@ -1,51 +1,66 @@
+function validarForm(){
+  var nombre = document.getElementById("nombre");
+  var apellidos = document.getElementById("apellidos");
+  var direccion = document.getElementById("direccion");
+  var telefono = document.getElementById("telefono");
 
-    function cargarEstilo(){
-    
-    //Agrego algo de estilo a la página
-    document.body.style.textAlign="left";
-    document.body.style.width="50%";
-    document.body.style.margin="auto";
-    document.body.style.backgroundColor="#F6BB69";
+  // Validar campos de datos obligatorios
+  if (nombre.value.trim() === "" || apellidos.value.trim() === "" || direccion.value.trim() === "") {
+    alert("Los campos de datos son obligatorios");
+    return false;
+  }
+
+  var numeroTelefono = telefono.value.trim();
+
+  // Validar que el número de teléfono es obligatorio
+  if (numeroTelefono === "") {
+    alert("El campo de teléfono es obligatorio");
+    return false;
+  }
+
+  // Validar que el número de teléfono tiene exactamente 9 dígitos
+  if (numeroTelefono.length !== 9 || isNaN(numeroTelefono)) {
+    alert("El número de teléfono debe tener nueve dígitos");
+    return false;
+  }
+
+  // Validar que se haya elegido un tamaño de pizza
+  var tamanio = document.getElementsByName("tamanio");
+  var seleccionTam = false;
+  for (var i = 0; i < tamanio.length; i++) {
+    if (tamanio[i].checked) {
+      seleccionTam = true;
+      break;
     }
+  }
+  if (!seleccionTam) {
+    alert("Debe seleccionar un tamaño de pizza");
+    return false;
+  }
 
-    function validarForm() {
-    var nombre = document.getElementById("nombre").value;
-    var apellidos = document.getElementById("apellidos").value;
-    var direccion = document.getElementById("direccion").value;
-    var telefono = document.getElementById("telefono").value;
-
-    if (nombre.trim() === "" || apellidos.trim() === "" || direccion.trim() === "" || telefono.trim() === "") {
-        alert("[ERROR] Todos los campos de texto deben estar rellenos.");
-        return false;
+  // Validar que se haya seleccionado al menos un ingrediente para la pizza
+  var ingredientes = document.getElementsByName("ingrediente");
+  var seleccionIng = false;
+  for (var i = 0; i < ingredientes.length; i++) {
+    if (ingredientes[i].checked) {
+      seleccionIng = true;
+      break;
     }
-    else
-      return true;
+  }
+  if (!seleccionIng) {
+    alert("Debe seleccionar al menos un ingrediente");
+    return false;
+  }
+
+  // Si todas las validaciones pasan, el formulario se envía
+  return true;
 }
 
-  //Validando qie se haya elegido una opción para el radio  
-  tamanio = document.getElementById("tamanio");
-  var seleccion = false;
-        for(var i=0; i<tamanio.length; i++) {
-            if(tamanio[i].checked) {
-                seleccion = true;
-                break;
-            }
-        }
-    
-       if(!seleccionado) {
-          alert("[ERROR] Debe seleccionar pequeña, mediana o grande)");
-          return false;
-      }
-
-
+// Asociar la función de validación con el evento "submit" del formulario
 window.onload = function () {
-    var formulario = document.getElementById("formulario");
-    formulario.onsubmit = validarForm;
-    cargarEstilo();
+  var formulario = document.getElementById("formulario");
+  formulario.onsubmit = validarForm;
+  };
 
-    }
 
-    function mostarPrecio(){
 
-      
-    }
